79dbef38fb2c40b325c7ba865c4d268afc7920fb
Jim Pivarski <jpivarski@gmail.com>
2023-05-18 18:39:17 -0500

    Put the name-lookups in a more convenient form.

---
+++ analysis/simplify-GHArchive-lookups.py
@@ -0,0 +1,38 @@
+import json
+
+import h5py
+import numpy as np
+
+for lookup in [
+    "repo", "actor", "org"
+]:
+    lines = set()
+
+    for filename in [
+        "GHArchive-2023.h5",
+        "GHArchive-2022.h5",
+        "GHArchive-2021.h5",
+        "GHArchive-2020.h5",
+        "GHArchive-2019.h5",
+        "GHArchive-2018.h5",
+        "GHArchive-2017.h5",
+        "GHArchive-2016.h5",
+        "GHArchive-2015.h5",
+    ]:
+        with h5py.File(filename, "r") as file:
+            for name, ids in json.loads(np.asarray(file[lookup + "_name2id_json"]).tobytes()).items():
+                for x in ids:
+                    lines.add(f"{x}\t{name}\n")
+        print(filename, len(lines))
+
+    lines = list(lines)
+    lines.sort()
+    print("sorted")
+
+    with open(lookup + "_id_name.txt", "w") as file:
+        for line in lines:
+            file.write(line)
+
+    print(lookup)
+
+print("DONE")
