58798125f2f23489238043dcefd1d8f3ed4ed226
jpivarski <jpivarski@gmail.com>
2023-05-23 17:41:01 -0700

    Start work on a smarter AST scan.

---
+++ analysis/smarter-ast-scan.py
@@ -0,0 +1,209 @@
+import ast
+import glob
+import json
+import tarfile
+from collections import Counter
+
+import jedi
+
+
+STDLIB_MODULES = {
+    '__main__',
+    # text processing
+    'string', 're', 'difflib', 'textwrap', 'unicodedata', 'stringprep', 'readline', 'rlcompleter',
+    # binary data services
+    'struct', 'codecs',
+    # data types
+    'datetime', 'calendar', 'collections', 'heapq', 'bisect', 'array', 'weakref', 'types', 'copy', 'pprint', 'reprlib', 'enum',
+    # numeric and mathematical modules
+    'numbers', 'math', 'cmath', 'decimal', 'fractions', 'random', 'statistics',
+    # functional programming modules
+    'itertools', 'functools', 'operator',
+    # file and directory access
+    'pathlib', 'fileinput', 'stat', 'filecmp', 'tempfile', 'glob', 'fnmatch', 'linecache', 'shutil', 'macpath',
+    # data persistence
+    'pickle', 'copyreg', 'shelve', 'marshal', 'dbm', 'sqlite3',
+    # data compression and archiving
+    'zlib', 'gzip', 'bz2', 'lzma', 'zipfile', 'tarfile',
+    # file formats
+    'csv', 'configparser', 'netrc', 'xdrlib', 'plistlib',
+    # crypographic services
+    'hashlib', 'hmac', 'secrets',
+    # generic operating system services
+    'os', 'io', 'time', 'argparse', 'getopt', 'logging', 'getpass', 'curses', 'platform', 'errno', 'ctypes',
+    # concurrent execution
+    'threading', 'multiprocessing', 'concurrent', 'subprocess', 'sched', 'queue', '_thread', '_dummy_thread', 'dummy_threading',
+    # contextvars
+    'contextvars',
+    # networking and interprocess communication
+    'asyncio', 'socket', 'ssl', 'select', 'selectors', 'asyncore', 'asynchat', 'signal', 'mmap',
+    # internet data handling
+    'email', 'json', 'mailcap', 'mailbox', 'mimetypes', 'base64', 'binhex', 'binascii', 'quopri', 'uu',
+    # structured markup processing tools
+    'html', 'xml',
+    # internet protocols and support
+    'webbrowser', 'cgi', 'cgitb', 'wsgiref', 'urllib', 'ftplib', 'poplib', 'imaplib', 'nntplib', 'smtplib', 'smtpd', 'telnetlib', 'uuid', 'socketserver', 'xmlrpc', 'ipaddress',
+    # multimedia
+    'audioop', 'aifc', 'sunau', 'wave', 'chunk', 'colorsys', 'imghdr', 'sndhdr', 'ossaudiodev',
+    # internationalization
+    'gettext', 'locale',
+    # program frameworks
+    'turtle', 'cmd', 'shlex',
+    # graphical user interfaces with tk
+    'tkinter',
+    # development tools
+    'typing', 'pydoc', 'doctest', 'unittest', 'lib2to3', 'test',
+    # debugging and profiling
+    'bdb', 'faulthandler', 'pdb', 'timeit', 'trace', 'tracemalloc',
+    # software packaging and distribution
+    'distutils', 'ensurepip', 'venv', 'zipapp',
+    # python runtime services
+    'sys', 'sysconfig', 'builtins', 'warnings', 'dataclasses', 'contextlib',
+    'abc', 'atexit', 'traceback', '__future__', 'gc', 'inspect', 'site',
+    # custom python interpreters
+    'code', 'codeop',
+    # importing modules
+    'zipimport', 'pkgutil', 'modulefinder', 'runpy', 'importlib',
+    # python language services
+    'parser', 'ast', 'symtable', 'symbol', 'token', 'keyword', 'tokenize', 'tabnanny', 'pyclbr', 'py_compile', 'compileall', 'dis', 'pickletools',
+    # miscellaneous services
+    'formatter',
+    # ms windows specific services
+    'msilib', 'msvcrt', 'winreg', 'winsound',
+    # unix specific services
+    'posix', 'pwd', 'spwd', 'grp', 'crypt', 'termios', 'tty', 'pty', 'fcntl', 'pipes', 'resource', 'nis', 'syslog',
+    # superseded modules
+    'optparse', 'imp',
+    # undocumented modules
+    'posixpath', 'ntpath'
+}
+
+
+class APIVisitor(ast.NodeVisitor):
+    def __init__(self, script):
+        self.script = script
+        self.results = {}
+
+    def _infer(self, node):
+        try:
+            out = []
+            for x in self.script.infer(node.lineno, node.end_col_offset):
+                if (
+                        not x.in_builtin_module()
+                        and x.full_name is not None
+                        and x.module_name.split(".")[0] not in STDLIB_MODULES
+                ):
+                    out.append((x.type, x.full_name))
+            return out
+        except:
+            return []
+
+    def _literal(self, node):
+        if node is None:
+            return ""
+
+        elif isinstance(node, ast.Constant):
+            if node.value in (None, False, True):
+                return str(node.value)
+            elif isinstance(node.value, int) and not isinstance(node.value, float):
+                if -10 <= node.value <= 10:
+                    return str(node.value)
+                else:
+                    return "int"
+            else:
+                return type(node.value).__name__
+
+        elif isinstance(node, ast.Slice):
+            step = self._literal(node.step)
+            if step == "":
+                return f"{self._literal(node.lower)}:{self._literal(node.upper)}"
+            else:
+                return f"{self._literal(node.lower)}:{self._literal(node.upper)}:{step}"
+
+        else:
+            return "?"
+
+    def visit_Attribute(self, node):
+        self.visit(node.value)
+        if not isinstance(node.ctx, ast.Store):
+            out = []
+            base_result = self.results.get((node.value.lineno, node.value.end_col_offset), [])
+            for kind, match in base_result:
+                out.append(("__getattr__", match + "." + node.attr))
+            self.results[node.lineno, node.end_col_offset] = out
+
+    def visit_Subscript(self, node):
+        self.visit(node.value)
+        self.visit(node.slice)
+        if not isinstance(node.ctx, ast.Store):
+            if isinstance(node.slice, ast.Tuple):
+                trailer = "[" + ", ".join(self._literal(x) for x in node.slice.elts) + "]"
+            else:
+                trailer = "[" + self._literal(node.slice) + "]"
+
+            out = []
+            for kind, match in self.results[node.value.lineno, node.value.end_col_offset]:
+                out.append(("__getitem__", match + trailer))
+            self.results[node.lineno, node.end_col_offset] = out
+
+    def visit_Name(self, node):
+        if not isinstance(node.ctx, ast.Store):
+            self.results[node.lineno, node.end_col_offset] = self._infer(node)
+
+    def visit_Call(self, node):
+        self.visit(node.func)
+        for x in node.args:
+            self.visit(x)
+        for x in node.keywords:
+            self.visit(x)
+
+        args = []
+        for x in node.args:
+            if not isinstance(x, ast.Starred):
+                args.append(self._literal(x))
+        for x in sorted([x for x in node.keywords if x.arg is not None], key=lambda x: x.arg):
+            args.append(x.arg + "=" + self._literal(x.value))
+        if any(isinstance(x, ast.Starred) for x in node.args) or any(x.arg is None for x in node.keywords):
+            args.append("*whatever")
+        trailer = "(" + ", ".join(args) + ")"
+
+        out = []
+        for kind, match in self.results[node.func.lineno, node.func.end_col_offset]:
+            out.append(("__call__", match + trailer))
+        self.results[node.func.lineno, node.func.end_col_offset] = out
+
+        self.results[node.lineno, node.end_col_offset] = self._infer(node)
+
+
+counter = Counter()
+
+project = jedi.Project("/Users/jpivarski/irishep/awkward/src/awkward")
+
+for filename in glob.glob("/Users/jpivarski/storage/data/GitHub-CMSSW-user-nonfork-raw-data-1Mcut-awkward/*/*.tgz"):
+    print(filename)
+    with tarfile.open(filename) as tgz:
+        for info in tgz:
+            if info.name.endswith(".py"):
+                print("    " + info.name)
+
+                with tgz.extractfile(info) as file:
+                    full_text = file.read()
+
+                try:
+                    syntax_tree = ast.parse(full_text)
+                except:
+                    continue
+
+                script = jedi.Script(full_text, project=project)
+
+                visitor = APIVisitor(script)
+                visitor.visit(syntax_tree)
+
+                for (line, column), matches in visitor.results.items():
+                    for match in matches:
+                        counter[match] += 1 / len(matches)
+
+    break
+
+for k, v in sorted(counter.items(), key=lambda pair: pair[1]):
+    print(f"{v:10.3f} {k}")
